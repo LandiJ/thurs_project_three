02d51011fa10d4705d33e21f221c816ed1f27448
Alandryia Jones <jonesalandryia@gmail.com>
2017-06-08 17:22:23 -0500

    the header still needs work but the app is coming together

--- main.js
+++ main.js
@@ -87,11 +87,42 @@ function createTextInput() {
       formData[i].type === "email"
     ) {
       var newInput = document.createElement("input");
-      newInput.setAttribute("type", "text");
+      newInput.setAttribute("type", formData.type);
       newInput.setAttribute("placeholder", formData[i].label);
 
       form.appendChild(newInput);
+    } else if (formData[i].type === "select") {
+      var select = document.createElement("select");
+      var defaultOption = document.createElement("option");
+      //   defaultOption.value = formData[i].options[j].value;
+      defaultOption.textContent = formData[i].label;
+      defaultOption.default = true;
+      select.appendChild(defaultOption);
+
+      for (var j = 0; j < formData[i].options.length; j++) {
+        var option = document.createElement("option");
+        option.value = formData[i].options[j].value;
+        option.textContent = formData[i].options[j].label;
+
+        select.appendChild(option);
+      }
+      form.appendChild(select);
+    } else if (formData[i].type === "textarea") {
+      var textarea = document.createElement("textarea");
+      textarea.setAttribute("placeholder", "Your Comment");
+      textarea.setAttribute("id", "user-comment");
+      form.appendChild(textarea);
     }
   }
 }
 createTextInput();
+function addSubmit() {
+  var form = document.querySelector("form");
+  var submitButton = document.createElement("input");
+  submitButton.setAttribute("type", "submit");
+  submitButton.setAttribute("value", "Submit Form");
+  submitButton.setAttribute("id", "submit");
+  form.appendChild(submitButton);
+}
+
+addSubmit();
